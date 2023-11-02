0e3c5ad934dc95a3bbd619b2784aa90243c8f5d2
eme-web <udobongeme4@gmail.com>
2023-11-02 16:19:24 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import jsonServer from "json-server";
+import auth from "json-server-auth";
+import cors from "cors";
+
+const server = express();
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Header', '*')
+    next()
+})
+
+const router = jsonServer.router('./data/db.json');
+server.use('/api', router);
+server.use(
+    cors({
+        origin: "https://codebook-snowy.vercel.app/"
+    })
+)
+server.db = router.db
+
+const middlewares = jsonServer.defaults()
+const rules = auth.rewriter({
+    products: 444,
+    featured_products: 444,
+    orders: 660,
+    users: 600
+})
+
+server.use(rules)
+server.use(auth)
+server.use(middlewares)
+server.use(router)
+
+server.listen(8000);
